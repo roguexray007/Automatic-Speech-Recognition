9990d99959156d6a336ae02a36a27f77a0181cdc
Apoorva Joshi <xunl3kah@gmail.com>
2018-04-09 01:38:19 +0530

    Update dynamic_brnn.py

--- models/dynamic_brnn.py
+++ models/dynamic_brnn.py
@@ -1,9 +1,5 @@
 # encoding: utf-8
 # ******************************************************
-# Author       : zzw922cn
-# Last modified: 2017-12-09 11:00
-# Email        : zzw922cn@gmail.com
-# Filename     : dynamic_brnn.py
 # Description  : Dynamic Bidirectional RNN model for Automatic Speech Recognition
 # ******************************************************
 
